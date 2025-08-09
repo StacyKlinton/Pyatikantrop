b27624cc406a693ef384b039d1d20491af1e186c
StacyKlinton <stacyklinton@gmail.com>
2025-08-09 21:38:15 +0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,9 +1,5 @@
-// src/firebaseConfig.js
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
-import { getAuth } from "firebase/auth";
-
-const DEFAULT_FIREBASE_CONFIG = {
+// firebaseConfig.js (в корне репо)
+export const DEFAULT_FIREBASE_CONFIG = {
   apiKey: "AIzaSyCPhzohvNOW77WONxPpgK9mCPXl84CGOEc",
   authDomain: "pyatikantrop-game.firebaseapp.com",
   databaseURL: "https://pyatikantrop-game-default-rtdb.firebaseio.com",
@@ -13,6 +9,4 @@ const DEFAULT_FIREBASE_CONFIG = {
   appId: "1:421071557643:web:56046f427ee65cb452a30e"
 };
 
-const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
-export const auth = getAuth(app);
+
